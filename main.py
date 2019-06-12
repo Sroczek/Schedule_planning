a310aed3dbd7d4ff5d2bda05c90162ac17258fda
Jakub Sroka <jakubsroka3@gmail.com>
2019-06-12 17:00:28 +0200

    lol

--- main.py
+++ main.py
@@ -1,108 +1,27 @@
-import argparse
-from sheet_search import SheetSearch
-import intervals as I
-import datetime
-
-# parser = argparse.ArgumentParser(description="Program służy do wyszukiwania terminów na które można przełożyć dane zajęcia\n")
-#
-# parser.add_argument('-p', '--page', help="Nazwa zakładki arkusza", required=True)
-# parser.add_argument('-r', '--row', help="Wiersz dla którego zamiany wyszukać", type=int, required=True)
-#
-#
-# argv = parser.parse_args()
-# row = argv.row
-# semestr = argv.page
-
-begin_classes_time = datetime.time(hour=8, minute=0)
-end_classes_time = datetime.time(hour=19, minute=20)
-days_of_week = ('Pn', 'Wt', 'Sr', 'Cz', 'Pt', 'Sb', 'Nd')
-class_duration = 90
-
-
-def parse_query(query):
-    argv = query.split(" ")
-    if argv[0] not in ['zima-s', 'zima-n', 'lato-s', 'lato-n']:
-        raise ValueError("Pierwszy argument powinien być nazwą zakładki\n[zima-s | zima-n | lato-s | lato-n]")
-    return argv[0], int(argv[1])
-
-
-def dtoi(d: datetime.time):
-    return d.hour*60 + d.minute
-
-
-def itod(i: int):
-    return datetime.time(hour=i//60, minute=i%60)
-
-
-def blocking_row_to_interval(br):
-    if br[0] not in days_of_week or br[1] is None: return []
-    if br[2] is None: return [br[0], I.closedopen(dtoi(br[1]), dtoi(br[1]) + 90)]
-    return [br[0], I.closedopen(dtoi(br[1]), dtoi(br[2]))]
-
-
-def execute(ss, query):
-    ws, row = parse_query(query)
-    br = ss.get_blocking_rows(ws, row)
-    br = list(filter(lambda x: x != [], map(lambda x: blocking_row_to_interval(x), br)))
-
-    days_unreserved_hours = {day: I.closed(dtoi(begin_classes_time), dtoi(end_classes_time)) for day in days_of_week}
-
-    for interval in br:
-        days_unreserved_hours[interval[0]] = days_unreserved_hours[interval[0]] - interval[1]
-
-    pr = ss.get_proper_rooms(ws, row)
-    brr = ss.get_blocking_rows_for_rooms(ws, pr)
-
-    result = []
-
-    for room, intervals_when_blocked in brr.items():
-        days_unreserved_room_hours = {day: I.closed(dtoi(begin_classes_time), dtoi(end_classes_time)) for day in days_of_week}
-
-        intervals_when_blocked = list(filter(lambda x: x != [], map(lambda x: blocking_row_to_interval(x), intervals_when_blocked)))
-        for interval in intervals_when_blocked:
-            days_unreserved_room_hours[interval[0]] = days_unreserved_room_hours[interval[0]] - interval[1]
-
-        for day in days_of_week:
-            possible_hours = days_unreserved_hours[day] & days_unreserved_room_hours[day]
-            result += [(room, day, interv) for interv in list(possible_hours)]
-
-    result = list(map(lambda x: "{} {} {} {}".format(x[0], x[1], itod(x[2].lower), itod(x[2].upper)), filter(lambda y: y[2].upper - y[2].lower >= class_duration, result)))
-    return result
-
+from sheet_search import SheetSearch, itod
 
 ss = SheetSearch("sheet.xlsx")
 
 query = input(">>")
 while query != "q":
 
-    try:
-        res = execute(ss, query)
-    except ValueError as ex:
-        print(ex.args[0])
+    argv = query.split(" ")
+    if argv[0] not in ['zima_s', 'zima_n', 'lato_s', 'lato_n']:
+        print("Pierwszy argument powinien być nazwą zakładki\n[zima_s | zima_n | lato_s | lato_n]")
+    elif argv.__len__() == 1:
+        print("Oczekiwano dwóch argumnetów: nazwa zeszytu, numer wiersza")
     else:
-        for el in res :
-            print(el)
+        try:
+            res = ss.find_possible_hours(argv[0], int(argv[1]))
+        except KeyError:
+             print("Nazwy kolumn uległy zmianie. Konieczna jest modyfikacja oprogramowania")
+        except ValueError:
+            print("Drugi argument powiniem być numerem wiersza")
+        except IndexError:
+            print("Wiersz o podanym numerze nie istnieje")
+        else:
+            for x in res:
+                print("{} {}    {} - {}   {}".format(x[0], x[1], itod(x[2].lower), itod(x[2].upper), x[2].upper - x[2].lower))
+
     query = input(">>")
 
-
-'''pytania do Garka:
-1.
-czy możemy założyć, że 
-niestacjonarne => Sb | Nd
-stacjonarne => Pn | Wt | Sr | Cz | Pt
-
-2.
-w jaki sposób ma być sformatowane wyjście
-
-3.
-Jakie godziny -> ciagłe czy dyskretne? 
-Może opcja uruchomienia programu...? 
-
-4.
-Cay skrypt do jednokrotnego uruchomienia czy tryb interaktywny? 
-
-5.
-Może opcja do reloadowania? 
-
-
-'''
--- sheet_search.py
+++ sheet_search.py
@@ -1,8 +1,32 @@
 from openpyxl import load_workbook
 from collections import defaultdict
+import intervals as I
+import datetime
+
+begin_classes_time = datetime.time(hour=8, minute=0)
+end_classes_time = datetime.time(hour=19, minute=20)
+
+day_map = {'Pn':0, 'Wt':1, 'Sr':2, 'Cz':3, 'Pt':4, 'Sb': 5, 'Nd':6}
+
+
+def dtoi(d: datetime.time):
+    return d.hour * 60 + d.minute
+
+
+def itod(i: int):
+    return datetime.time(hour=i // 60, minute=i % 60).strftime("%H:%M")
+
+
+def blocking_row_to_interval(br):
+    if br[0] is None or br[1] is None:
+        return []
+    if br[2] is None:
+        return [br[0], I.closedopen(dtoi(br[1]), dtoi(br[1]) + 90)]
+    return [br[0], I.closedopen(dtoi(br[1]), dtoi(br[2]))]
 
 
 class SheetSearch:
+
     def __rows_generator(self, ws):
         for i, row in enumerate(ws.iter_rows(min_row=2, max_col=16)):
             yield [i + 2] + [cell.value for cell in row]
@@ -27,12 +51,9 @@ class SheetSearch:
     def get_blocking_rows(self, ws, row_no):
         """Metoda, ktora zwraca wszystkie terminy, w ktorych prowadzacy lub dany rocznik jest zajety"""
         magic_row = list(filter(lambda x: x["numer"] == row_no, list(self.rdd_dict[ws])))[0]
-        return list(
-            set(map(lambda x: (x["dzien"], x["godz"], x["koniec"]), filter(lambda x: x["osoba"] == magic_row["osoba"] or
-                                                                                     (x["studia"] == magic_row[
-                                                                                         "studia"] and x["sem"] ==
-                                                                                      magic_row["sem"]),
-                                                                           self.rdd_dict[ws]))))
+        return list(map(lambda x: blocking_row_to_interval((x["dzien"], x["godz"], x["koniec"])),
+                    filter(lambda x: x["osoba"] == magic_row["osoba"] or (x["studia"] == magic_row["studia"]
+                                                                          and x["sem"] ==magic_row["sem"]), self.rdd_dict[ws])))
 
     def get_proper_rooms(self, ws, row_no):
         """Metoda, ktora zwraca nazwy wszystkich sal, w ktorych moga odbyc sie dane zajecia"""
@@ -46,13 +67,63 @@ class SheetSearch:
         help = list(set(map(lambda x: (x["sala"], x["dzien"], x["godz"], x["koniec"]),
                             filter(lambda x: x["sala"] in room_list, self.rdd_dict[ws]))))
         result = defaultdict(list)
+        for x in room_list:
+            result[x] = []
         for i, j, k, l in help:
-            result[i].append((j, k, l))
+            result[i].append(blocking_row_to_interval((j, k, l)))
         return result
 
     def get_row(self, ws, row_no):
         magic_row = list(filter(lambda x: x["numer"] == row_no, list(self.rdd_dict[ws])))[0]
         return magic_row
+
+    def find_possible_hours(self, ws, row_no):
+        row = self.get_row(ws, row_no)
+        class_duration = dtoi(row["koniec"]) - dtoi(row["godz"]) if row["koniec"] is not None else 90
+
+        print("Wyszukiwanie: {}, {}, {}, {}".format(row["przedmiot"], row["osoba"], row["sala"], row["godz"]))
+
+        if ws[-1] == 's':
+            days = ('Pn', 'Wt', 'Sr', 'Cz', 'Pt')
+        elif ws[-1] == 'n':
+            days = ('Sb', 'Nd')
+
+        br = self.get_blocking_rows(ws, row_no)
+
+        days_unreserved_hours = {day: I.closed(dtoi(begin_classes_time), dtoi(end_classes_time)) for day in days}
+
+        for interval in br:
+            if interval:
+                days_unreserved_hours[interval[0]] = days_unreserved_hours[interval[0]] - interval[1]
+
+        pr = self.get_proper_rooms(ws, row_no)
+        brr = self.get_blocking_rows_for_rooms(ws, pr)
+        brr2 = self.get_blocking_rows_for_rooms(ws[:-1] + "inne", pr)
+        for key, value in brr.items():
+            for key2, value2 in brr2.items():
+                if key == key2:
+                    value += value2
+        result = []
+
+        for room, intervals_when_blocked in brr.items():
+
+            days_unreserved_room_hours = {day: I.closed(dtoi(begin_classes_time), dtoi(end_classes_time)) for day in
+                                          days}
+
+            for interval in intervals_when_blocked:
+                if interval and interval[0] in days:
+                    days_unreserved_room_hours[interval[0]] = days_unreserved_room_hours[interval[0]] - interval[1]
+
+            for day in days:
+                possible_hours = days_unreserved_hours[day] & days_unreserved_room_hours[day]
+                result += [(room, day, interv) for interv in list(possible_hours)]
+
+        result = list(filter(lambda y: y[2].upper - y[2].lower >= class_duration, result))
+        result = sorted(result, key=lambda x: x[2])
+        result = sorted(result, key=lambda x: day_map[x[1]])
+
+
+        return result
 
     def __init__(self, path):
         wb = load_workbook(filename=path, read_only=True)
@@ -89,4 +160,3 @@ if __name__ == '__main__':
         print(key)
         for v in value:
             print(v)
-
