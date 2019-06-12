61ce275450ac14028e50d9cdb10907b57fc5e32e
pikinier20 <filip.zybala@gmail.com>
2019-06-12 17:56:25 +0200

    commit

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from sheet_search import SheetSearch, itod
+from openpyxl.utils.exceptions import InvalidFileException
+from sheet_search import SheetSearch, itod, cm
 
-ss = SheetSearch("sheet.xlsx")
 
+print("Schedule Planning V1.0\nAplikacja ułatwiająca przenoszenie zajęć\n")
+sheet_name = input("Podaj ścieżkę do arkusza:\n>>")
+if not sheet_name:
+    sheet_name = "sheet.xlsx"
+try:
+    ss = SheetSearch(sheet_name)
+except (InvalidFileException,FileNotFoundError):
+    print("Ścieżka do pliku niepoprawna")
+    exit(0)
+print("Arkusz został załadowany. Podaj wiersz w formacie: <nazwa_zakladki> <nr_wiersza>")
 query = input(">>")
 while query != "q":
 
     argv = query.split(" ")
-    if argv[0] not in ['zima_s', 'zima_n', 'lato_s', 'lato_n']:
-        print("Pierwszy argument powinien być nazwą zakładki\n[zima_s | zima_n | lato_s | lato_n]")
-    elif argv.__len__() == 1:
-        print("Oczekiwano dwóch argumnetów: nazwa zeszytu, numer wiersza")
-    else:
-        try:
-            res = ss.find_possible_hours(argv[0], int(argv[1]))
-        except KeyError:
-             print("Nazwy kolumn uległy zmianie. Konieczna jest modyfikacja oprogramowania")
-        except ValueError:
-            print("Drugi argument powiniem być numerem wiersza")
-        except IndexError:
-            print("Wiersz o podanym numerze nie istnieje")
+    if argv[0] == 'find':
+        if argv[1] not in [cm['zima_s'], cm['zima_n'], cm['lato_s'], cm['lato_n']]:
+            print("Pierwszy argument powinien być nazwą zakładki\n[%s | %s | %s | %s]" % (cm['zima_s'], cm['zima_n'], cm['lato_s'], cm['lato_n']))
+        elif argv.__len__() < 3:
+            print("Oczekiwano dwóch argumentów: nazwa zeszytu, numer wiersza")
         else:
-            for x in res:
-                print("{} {}    {} - {}   {}".format(x[0], x[1], itod(x[2].lower), itod(x[2].upper), x[2].upper - x[2].lower))
-
+            try:
+                res = ss.find_possible_hours(argv[1], int(argv[2]))
+            except KeyError as k:
+                print(k)
+                print("Nazwy kolumn uległy zmianie. Konieczna jest modyfikacja oprogramowania")
+            except ValueError:
+                print("Drugi argument powiniem być numerem wiersza")
+            except IndexError:
+                print("Wiersz o podanym numerze nie istnieje")
+            else:
+                for x in res:
+                    print("{} {}    {} - {}   {}".format(x[0], x[1], itod(x[2].lower), itod(x[2].upper), x[2].upper - x[2].lower))
+    else:
+        print("Obslugiwane komendy: find")
     query = input(">>")
 
--- sheet_search.py
+++ sheet_search.py
@@ -6,7 +6,28 @@ import datetime
 begin_classes_time = datetime.time(hour=8, minute=0)
 end_classes_time = datetime.time(hour=19, minute=20)
 
-day_map = {'Pn':0, 'Wt':1, 'Sr':2, 'Cz':3, 'Pt':4, 'Sb': 5, 'Nd':6}
+day_map = {'Pn': 0, 'Wt': 1, 'Sr': 2, 'Cz': 3, 'Pt': 4, 'Sb': 5, 'Nd': 6}
+
+cm = {
+    'bud': 'bud',  # Nazwa budynku w zeszycie "sale"
+    'nr': 'nr',  # Nazwa sali w zeszycie "sale"
+    'typ': 'typ',  # Typ sali w zeszycie "sale"
+    'dzien': 'dzien', # Dzien zajec w zeszytach zajec
+    'godz': 'godz',  # Godzina zajec w zeszytach zajec
+    'koniec': 'koniec',  # Koniec zajec w zeszytach zajec
+    'osoba': 'osoba',  # Osoba prowadzaca zajecia
+    'studia': 'studia',  # Rodzaj studiow
+    'sem': 'sem',  # Nr semestru
+    'sala': 'sala',
+    'przedmiot': 'przedmiot',  # Nazwa przedmiotu w zeszytach zajec
+    'zima_s': 'zima-s',  # Zeszyt zawierajacy zajecia zimowe stacjonarne
+    'zima_n': 'zima_n',  # Zeszyt zawierajacy zajecia zimowe niestacjonarne
+    'lato_s': 'lato_s',  # Zeszyt zawierajacy zajecia letnie stacjonarne
+    'lato_n': 'lato_n',  # Zeszyt zawierajacy zajecia letnie niestacjonarne
+    'zima_inne': 'zima_inne',  # Zeszyt zawierajacy zajecia zimowe inne
+    'lato_inne': 'lato_inne',  # Zeszyt zawierajacy zajecia letnie inne
+    'sale': 'sale'  # Zeszyt zawierajacy sale w ktorych moga odbywac sie zajecia
+}
 
 
 def dtoi(d: datetime.time):
@@ -43,29 +64,29 @@ class SheetSearch:
     def __map_rooms(self, ws):
         gen = self.__map_data(ws)
         for row in gen:
-            if row["bud"] == "D17" or row["bud"] == "D8":
-                yield {"nazwa": ((row["bud"] if row["bud"] is not None else "") + " " + (
-                    row["nr"] if row["nr"] is not None else "")).strip(),
-                       "typ": row["typ"] if row["typ"] is not None else ""}
+            if row[cm["bud"]] == "D17" or row[cm["bud"]] == "D8":
+                yield {"nazwa": ((row[cm["bud"]] if row[cm["bud"]] is not None else "") + " " + (
+                    row[cm["nr"]] if row[cm["nr"]] is not None else "")).strip(),
+                       "typ": row[cm["typ"]] if row[cm["typ"]] is not None else ""}
 
     def get_blocking_rows(self, ws, row_no):
         """Metoda, ktora zwraca wszystkie terminy, w ktorych prowadzacy lub dany rocznik jest zajety"""
         magic_row = list(filter(lambda x: x["numer"] == row_no, list(self.rdd_dict[ws])))[0]
-        return list(map(lambda x: blocking_row_to_interval((x["dzien"], x["godz"], x["koniec"])),
-                    filter(lambda x: x["osoba"] == magic_row["osoba"] or (x["studia"] == magic_row["studia"]
-                                                                          and x["sem"] ==magic_row["sem"]), self.rdd_dict[ws])))
+        return list(map(lambda x: blocking_row_to_interval((x[cm["dzien"]], x[cm["godz"]], x[cm["koniec"]])),
+                    filter(lambda x: x[cm["osoba"]] == magic_row[cm["osoba"]] or (x[cm["studia"]] == magic_row[cm["studia"]]
+                                                                          and x[cm["sem"]] ==magic_row[cm["sem"]]), self.rdd_dict[ws])))
 
     def get_proper_rooms(self, ws, row_no):
         """Metoda, ktora zwraca nazwy wszystkich sal, w ktorych moga odbyc sie dane zajecia"""
         magic_row = list(filter(lambda x: x["numer"] == row_no, self.rdd_dict[ws]))[0]
         return list(
-            set(map(lambda x: x["nazwa"], filter(lambda x: x["typ"] == magic_row["typ"], self.rdd_dict["sale"]))))
+            set(map(lambda x: x["nazwa"], filter(lambda x: x[cm["typ"]] == magic_row[cm["typ"]], self.rdd_dict[cm["sale"]]))))
 
     def get_blocking_rows_for_rooms(self, ws, room_list):
         """Metoda, ktora zwraca slownik przypisujacy kazdej sali trojki: dzien,godzina,koniec,
          w ktorych odbywaja sie zajecia"""
-        help = list(set(map(lambda x: (x["sala"], x["dzien"], x["godz"], x["koniec"]),
-                            filter(lambda x: x["sala"] in room_list, self.rdd_dict[ws]))))
+        help = list(set(map(lambda x: (x[cm["sala"]], x[cm["dzien"]], x[cm["godz"]], x[cm["koniec"]]),
+                            filter(lambda x: x[cm["sala"]] in room_list, self.rdd_dict[ws]))))
         result = defaultdict(list)
         for x in room_list:
             result[x] = []
@@ -79,9 +100,9 @@ class SheetSearch:
 
     def find_possible_hours(self, ws, row_no):
         row = self.get_row(ws, row_no)
-        class_duration = dtoi(row["koniec"]) - dtoi(row["godz"]) if row["koniec"] is not None else 90
+        class_duration = dtoi(row[cm["koniec"]]) - dtoi(row[cm["godz"]]) if row[cm["koniec"]] is not None else 90
 
-        print("Wyszukiwanie: {}, {}, {}, {}".format(row["przedmiot"], row["osoba"], row["sala"], row["godz"]))
+        print("Wyszukiwanie: {}, {}, {}, {}".format(row[cm["przedmiot"]], row[cm["osoba"]], row[cm["sala"]], row[cm["godz"]]))
 
         if ws[-1] == 's':
             days = ('Pn', 'Wt', 'Sr', 'Cz', 'Pt')
@@ -98,7 +119,9 @@ class SheetSearch:
 
         pr = self.get_proper_rooms(ws, row_no)
         brr = self.get_blocking_rows_for_rooms(ws, pr)
-        brr2 = self.get_blocking_rows_for_rooms(ws[:-1] + "inne", pr)
+        #brr2 = self.get_blocking_rows_for_rooms(ws[:-1] + "inne", pr)
+        brr2 = self.get_blocking_rows_for_rooms(cm['zima_inne'], pr) if ws == cm['zima_n'] or ws == cm['zima_s'] \
+            else self.get_blocking_rows_for_rooms(cm['lato_inne'], pr)
         for key, value in brr.items():
             for key2, value2 in brr2.items():
                 if key == key2:
@@ -119,44 +142,43 @@ class SheetSearch:
                 result += [(room, day, interv) for interv in list(possible_hours)]
 
         result = list(filter(lambda y: y[2].upper - y[2].lower >= class_duration, result))
-        result = sorted(result, key=lambda x: x[2])
+        result = sorted(result, key=lambda x: x[2].lower)
         result = sorted(result, key=lambda x: day_map[x[1]])
-
 
         return result
 
     def __init__(self, path):
         wb = load_workbook(filename=path, read_only=True)
         self.rdd_dict = {
-            "zima_s": [x for x in self.__map_data(wb["zima_s"])],
-            "lato_s": [x for x in self.__map_data(wb["lato_s"])],
-            "zima_n": [x for x in self.__map_data(wb["zima_n"])],
-            "lato_n": [x for x in self.__map_data(wb["lato_n"])],
-            "zima_inne": [x for x in self.__map_data(wb["zima_inne"])],
-            "lato_inne": [x for x in self.__map_data(wb["lato_inne"])],
-            "sale": [x for x in self.__map_rooms(wb["sale"])]
+            cm["zima_s"]: [x for x in self.__map_data(wb[cm["zima_s"]])],
+            cm["lato_s"]: [x for x in self.__map_data(wb[cm["lato_s"]])],
+            cm["zima_n"]: [x for x in self.__map_data(wb[cm["zima_n"]])],
+            cm["lato_n"]: [x for x in self.__map_data(wb[cm["lato_n"]])],
+            cm["zima_inne"]: [x for x in self.__map_data(wb[cm["zima_inne"]])],
+            cm["lato_inne"]: [x for x in self.__map_data(wb[cm["lato_inne"]])],
+            cm["sale"]: [x for x in self.__map_rooms(wb[cm["sale"]])]
         }
 
 
 # Przyklad uzycia
-
-if __name__ == '__main__':
-    ss = SheetSearch("sheet3.xlsx")
-    br = ss.get_blocking_rows("zima_s", 10)
-    pr = ss.get_proper_rooms("zima_s", 10)
-    brr = ss.get_blocking_rows_for_rooms("zima_inne", pr)
-    brr2 = ss.get_blocking_rows_for_rooms("zima_s", pr)
-    print(ss.get_row("zima_s",10))
-    print("Zajete terminy:")
-    for item in br:
-        print(item)
-    print('\n')
-    print("Zajetosc sal:")
-    for key, value in brr.items():
-        for key2, value2 in brr2.items():
-            if key == key2:
-                value += value2
-    for key, value in brr.items():
-        print(key)
-        for v in value:
-            print(v)
+#
+# if __name__ == '__main__':
+#     ss = SheetSearch("sheet3.xlsx")
+#     br = ss.get_blocking_rows("zima_s", 10)
+#     pr = ss.get_proper_rooms("zima_s", 10)
+#     brr = ss.get_blocking_rows_for_rooms("zima_inne", pr)
+#     brr2 = ss.get_blocking_rows_for_rooms("zima_s", pr)
+#     print(ss.get_row("zima_s",10))
+#     print("Zajete terminy:")
+#     for item in br:
+#         print(item)
+#     print('\n')
+#     print("Zajetosc sal:")
+#     for key, value in brr.items():
+#         for key2, value2 in brr2.items():
+#             if key == key2:
+#                 value += value2
+#     for key, value in brr.items():
+#         print(key)
+#         for v in value:
+#             print(v)
